83f482d13acde0e63bcbdae1b764a9b1b9d51319
Kevin Reynolds <Kevin.T.Reynolds30@gmail.com>
2017-04-21 23:13:36 -0700

    add work, project, bio and education objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,88 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+  "name" : "Kevin Reynolds",
+  "role" : "Web Developer",
+  "contactInfo" : {
+    "mobile" : "480-262-8126",
+    "email" : "Kevin.T.Reynolds30@gmail.com",
+    "github" : "OnlinePseudonym",
+    "location" : "Phoenix"
+  },
+  "welcomeMessage" : "Welcome to my life",
+  "skills" : [
+    "JS", "HTML", "CSS", "Python"
+  ],
+  "bioPic" : "https://placehold.it/300x300"
+};
+
+var work = {
+  "jobs":[
+    {
+      "employer":"Florencia",
+      "title":"Kitchen Manager",
+      "dates":"November 1999 - present",
+      "location":"Phoenix, AZ",
+      "description": "Worked with a team to meet constantly changing goals and deadlines, provided customer service solutions as problems arouse, and effictively communicated the desires of staff and customers."
+    },
+    {
+      "employer":"Tempe Abrassives",
+      "title":"Customer Service",
+      "dates":"March 2008-August 2009",
+      "location":"Tempe, AZ",
+      "description": "Customer service, deliveries, accounting, inside sales."
+    }
+  ]
+};
+
+var education = {
+  "schools": [
+    {
+      "name":"Desert Vista High School",
+      "location":"Phoenix, AZ",
+      "degree":"High School Diploma",
+      "majors":["Gen ED"],
+      "dates":"Sept, 1999 - May, 2004",
+      "url":"http://www.tempeunion.org/desertvista"
+    }
+  ],
+  "onlineCourses": [
+    {
+      "title":"Intro to Computer Science",
+      "school":"Udacity",
+      "dates":"Feb, 2017",
+      "url":"https://www.udacity.com/course/intro-to-computer-science--cs101"
+    },
+    {
+      "title":"Intro to Programming Nanodegree",
+      "school":"Udacity",
+      "dates":"Feb, 2017",
+      "url":"https://www.udacity.com/course/intro-to-programming-nanodegree--nd000"
+    },
+    {
+      "title":"Frontend Web Developer Nanodegree",
+      "school":"Udacity",
+      "dates":"Feb, 2017 - Present",
+      "url":"https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+    }
+  ]
+};
+
+var projects = {
+  "projects":[
+    {
+      "title":"Favorite Movies Website",
+      "dates":"March 2017",
+      "description":"A simple website listing a few of my favorite movies, displaying a trailer if you click on the box art and offering a link to reviews.",
+      "images":[
+        "images/fresh_tomatoes_1.jpg","images/fresh_tomatoes_2.jpg"
+      ]
+    },
+    {
+      "title":"Portfolio Website",
+      "dates":"April 2017",
+      "description":"A streamlined portfolio showcasing some recent projects.",
+      "images":[
+        "images/...jpg","images/...jpg"
+      ]
+    }
+  ]
+}
